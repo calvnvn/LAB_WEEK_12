076037f6eccc7889c78625c4e80b6ab0522b6e43
Calvin-braus <75593027+calvnvn@users.noreply.github.com>
2025-11-28 21:47:10 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
 
 class MovieViewModel(private val movieRepository: MovieRepository)
     : ViewModel() {
@@ -23,13 +24,29 @@ class MovieViewModel(private val movieRepository: MovieRepository)
 
     private val _error = MutableStateFlow("")
     val error: StateFlow<String> = _error
+    // fetch movies from the API
     private fun fetchPopularMovies() {
         viewModelScope.launch(Dispatchers.IO) {
-            movieRepository.fetchMovies().catch {
-                _error.value = "An exception occurred: ${it.message}"
-            }.collect {
-                _popularMovies.value = it
-            }
+            movieRepository.fetchMovies()
+                // --- BAGIAN BARU (ASSIGNMENT) ---
+                .map { movies ->
+                    // 1. Ambil tahun saat ini
+                    val currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR).toString()
+
+                    // 2. Lakukan Filter & Sorting seperti di Part 1
+                    movies.filter { movie ->
+                        movie.releaseDate?.startsWith(currentYear) == true
+                    }.sortedByDescending { movie ->
+                        movie.popularity
+                    }
+                }
+                // --------------------------------
+                .catch { exception ->
+                    _error.value = "An exception occurred: ${exception.message}"
+                }
+                .collect { movies ->
+                    _popularMovies.value = movies
+                }
         }
     }
 }
